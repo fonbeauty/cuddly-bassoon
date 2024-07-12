0b12c6e7db81d642a693cffa0713fcf2b68153df
bilbo <fonbeauty@mail.ru>
2024-07-12 19:29:09 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const loginForm = document.getElementById('login-form');
 const errorMessageDiv = document.getElementById('error-message');
+const username = document.getElementById('username')
+const password = document.getElementById('password')
 
 loginForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	const username = document.getElementById('username').value;
-	const password = document.getElementById('password').value;
 
-	if (username === 'admin' && password === 'password') {
+
+	if (username.value === 'admin' && password.value === 'password') {
 		alert('Login successful!');
 		location.href = 'index.html'; // redirect to another page
 	} else {
+		errorMessageDiv.style.color = "red"
 		errorMessageDiv.textContent = 'Invalid username or password';
 	}
 });
