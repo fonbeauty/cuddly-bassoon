abe535fb02f62c661a58e5392f16026f929c1f5b
bilbo <fonbeauty@mail.ru>
2024-12-09 10:51:08 +0300

    fix path to templates

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(prefix='/pages', tags=['Фронтенд'])
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory='app/templates')
 
 
 @router.get('/login')
